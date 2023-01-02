4208ab6e8853ea1c4ccf62bf8df8da8d53039176
Mithun B <mithun100@gmail.com>
2023-01-02 15:50:23 -0600

    AppDynamics configuration snippet is added

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
+//Please ensure to change the XXXX values with proper values.This snippet has the information to connect with AppDynamics controller
+require("appdynamics").profile({
+    controllerHostName: 'xxxx.saas.appdynamics.com',
+    controllerPort: 443, 
+    controllerSslEnabled: true,  // Set to true if controllerPort is SSL
+    accountName: 'xxxx',
+    accountAccessKey: 'xxxx', //required
+    applicationName: 'Mithun_Hello_World_Express',
+    tierName: 'Tier1', 
+    nodeName: 'Node1',
+    logging: {
+        logfiles: [
+             {'filename': 'nodejs_agent_%N.log', 'level': 'TRACE'},
+             {'filename': 'nodejs_agent_%N.protolog', 'level': 'TRACE', 'channel': 'protobuf'}
+         ]
+     } 
+   });
+   
+ 
 const express = require('express')
 const app = express()
 const port = 3000
